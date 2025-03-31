8f1932dcbebb90cae44285e463b0e6e26fbffc06
haasebr <167838407+haasebr@users.noreply.github.com>
2025-04-01 04:44:10 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/luci2/bin/config_generate
 
 # Modify hostname
 sed -i 's/LEDE/TR3000/g' package/base-files/luci2/bin/config_generate
